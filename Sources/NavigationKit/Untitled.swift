d0fad448aa3fe371c01b1ef4face93c9aed521d2
umavenkata1733 <umavenkata1733@gmail.com>
2025-04-24 09:39:37 -0400

    Update Untitled.swift

--- Sources/NavigationKit/Untitled.swift
+++ Sources/NavigationKit/Untitled.swift
@@ -1,89 +1,49 @@
-//
-//  Untitled.swift
-//  NavigationKit
-//
-//  Created by Anand on 1/14/25.
+import XCTest
+@testable import DropdownMenu // Replace with your module name
 
+final class DropdownDelegateTests: XCTestCase {
 
-import Combine
-import Foundation
-import SwiftUI
+    final class MockDelegate: DropdownDelegate {
+        var didSelectOptionCalled = false
+        var didDismissCalled = false
+        var selectedValue: String?
 
-public final class NavigationStore: ObservableObject {
-    @Published public var rootNode: AnyHashable
-    @Published public var navigationPath: [AnyHashable] = []
-    @Published public var childSheetNavigationStore: NavigationStore?
-    
-    public weak var parentNavigationStore: NavigationStore?
-
-    public init(
-        rootNode: AnyHashable,
-        parentNavigationStore: NavigationStore?
-    ) {
-        self.rootNode = rootNode
-        self.parentNavigationStore = parentNavigationStore
-    }
-    
-    public func openSheet(with node: AnyHashable) {
-        childSheetNavigationStore = NavigationStore(
-            rootNode: node,
-            parentNavigationStore: self
-        )
-    }
-    
-    public func closeSheet() {
-        parentNavigationStore?.childSheetNavigationStore = nil
-    }
-}
-
-
-//  NavRootView.swift
-//  NavigationFramework
-//
-//  Created by Anand on 1/14/25.
-//
-
-import SwiftUI
-
-// In NavigationFramework SPM package
-public struct NavRootView: View {
-    @ObservedObject private var navigationStore: NavigationStore
-    private let viewFactory: ViewFactory
-    
-    public init(navigationStore: NavigationStore, viewFactory: ViewFactory) {
-        self.navigationStore = navigationStore
-        self.viewFactory = viewFactory
-    }
-    
-    public var body: some View {
-        NavigationStack(path: $navigationStore.navigationPath) {
-            viewFactory.getView(for: navigationStore.rootNode)
-                .navigationDestination(for: AnyHashable.self) { node in
-                    viewFactory.getView(for: node)
-                }
+        func didSelectOption<T>(_ option: T) {
+            didSelectOptionCalled = true
+            selectedValue = option as? String
         }
-        .sheet(isPresented: .init(
-            get: { navigationStore.childSheetNavigationStore != nil },
-            set: { _ in navigationStore.childSheetNavigationStore = nil }
-        )) {
-            if let sheetNavigationStore = navigationStore.childSheetNavigationStore {
-                NavRootView(
-                    navigationStore: sheetNavigationStore,
-                    viewFactory: viewFactory
-                )
-            }
+
+        func didDismiss() {
+            didDismissCalled = true
         }
     }
-    
-    
-}
 
+    func testDidSelectOption() {
+        let mock = MockDelegate()
+        let anyDelegate = AnyDropdownDelegate<String>(mock)
 
-@MainActor
-open class ViewFactory {
-    public init() {}
-    @ViewBuilder
-    open func getView(for node: AnyHashable) -> some View {
-        Text("Override getView in your app's ViewFactory")
+        anyDelegate.didSelectOption("Test Option")
+
+        XCTAssertTrue(mock.didSelectOptionCalled)
+        XCTAssertEqual(mock.selectedValue, "Test Option")
+    }
+
+    func testDidDismiss() {
+        let mock = MockDelegate()
+        let anyDelegate = AnyDropdownDelegate<String>(mock)
+
+        anyDelegate.didDismiss()
+
+        XCTAssertTrue(mock.didDismissCalled)
+    }
+
+    func testDidSelectOptionWithWrongType() {
+        let mock = MockDelegate()
+        let anyDelegate = AnyDropdownDelegate<String>(mock)
+
+        anyDelegate.didSelectOption(123) // Int instead of String
+
+        XCTAssertFalse(mock.didSelectOptionCalled)
+        XCTAssertNil(mock.selectedValue)
     }
 }
